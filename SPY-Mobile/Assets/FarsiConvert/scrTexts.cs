86a08063c41bcb1feb1391dfe4e85262a1b424dd
Sara Eftekhari <sarabre26@gmail.com>
2022-05-08 06:12:37 -0700

    Revert "Go to page and back or whatever you like, in that way you will get page GameObject to ActionManager and it will send the name of GameObject to CanvasManager. In CanvasManager we have a list of pages (list of panel classes, each of which has a panel GameObject and property for the name, which automatically gets pulled from the name of the GameObject). At the end, the sent name is matched with the field name in the list of pages and the index is obtained and the GameObject is activated."
    This reverts commit da6a135dae754035c3ba3a1d882d7dc3628be806.

---
+++ SPY-Mobile/Assets/FarsiConvert/scrTexts.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class scrTexts : MonoBehaviour {
+
+
+	public string[] strList;
+	public GUISkin guiSkin;
+	public bool convert;
+
+	WordSpliter wordSpliter = new WordSpliter() ;
+	string s;
+
+	void Start()
+	{
+
+
+//		wordSpliter.SplitTheWord (lableInput.text, 15);
+		s = wordSpliter.SplitTheWord (lableInput.text, 4) ;
+		
+		lableInput.text = s;
+//		if(convert)
+//		SetPersianText ();
+
+	}
+	public bool isSelect;
+	public bool isDeselect;
+	public Input input;
+    public Text lableInput;
+
+	void InputText(string strInput)
+	{
+		lableInput.text = strInput;
+
+	}
+
+
+	void OnClick()
+	{
+		SetPersianText();
+		print("call");
+	}
+
+
+	public float right;
+	public float up;
+	public float scaleX;
+	public float scaleY;
+
+	public GUISkin skin;
+
+
+	public string text;
+
+
+	void OnGUI(){
+
+
+
+
+
+		GUI.Label (new Rect (Screen.width / 2 - right, Screen.height / 2 - up, scaleX, scaleY),lableInput.text);
+
+		}
+
+
+
+
+	public void SetPersianText()
+	{
+		InputText(lableInput.text.faConvert());
+
+	}
+
+
+}
