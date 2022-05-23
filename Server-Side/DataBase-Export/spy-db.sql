cdaceeba947660d07133851a55faf4a4848f7f5b
Sara Eftekhari <sarabre26@gmail.com>
2022-05-23 07:41:06 -0700

    Here is a database export with data for showing in Unity

--- Server-Side/DataBase-Export/spy-db.sql
+++ Server-Side/DataBase-Export/spy-db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 23, 2022 at 10:23 AM
+-- Generation Time: May 23, 2022 at 02:36 PM
 -- Server version: 5.7.36
 -- PHP Version: 7.4.26
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `spy-db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `username` varchar(10) NOT NULL,
+  `name` varchar(20) CHARACTER SET utf16 COLLATE utf16_persian_ci NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `gmail` varchar(70) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suggested-words`
+--
+
+DROP TABLE IF EXISTS `suggested-words`;
+CREATE TABLE IF NOT EXISTS `suggested-words` (
+  `Word` varchar(20) CHARACTER SET utf16 COLLATE utf16_persian_ci NOT NULL,
+  `wg-code` int(5) NOT NULL,
+  `wg-name` varchar(20) CHARACTER SET utf16 COLLATE utf16_persian_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -66,23 +93,37 @@ INSERT INTO `wg-02` (`ID`, `Word`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `wg-03`
+--
+
+DROP TABLE IF EXISTS `wg-03`;
+CREATE TABLE IF NOT EXISTS `wg-03` (
+  `ID` int(7) NOT NULL,
+  `Word` varchar(20) CHARACTER SET utf16 COLLATE utf16_persian_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `wordsgroup-list`
 --
 
 DROP TABLE IF EXISTS `wordsgroup-list`;
 CREATE TABLE IF NOT EXISTS `wordsgroup-list` (
-  `name-code` varchar(5) NOT NULL,
-  `name` varchar(20) CHARACTER SET utf16 COLLATE utf16_persian_ci NOT NULL,
-  `ID` int(5) NOT NULL
+  `name-code` text NOT NULL,
+  `name` text CHARACTER SET utf16 COLLATE utf16_persian_ci NOT NULL,
+  `ID` int(5) NOT NULL,
+  `Count` int(3) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `wordsgroup-list`
 --
 
-INSERT INTO `wordsgroup-list` (`name-code`, `name`, `ID`) VALUES
-('wg-01', 'پیش فرض', 50001),
-('wg-02', 'پزشکی', 50002);
+INSERT INTO `wordsgroup-list` (`name-code`, `name`, `ID`, `Count`) VALUES
+('wg-01', 'پیش فرض', 50001, 4),
+('wg-02', 'پزشکی', 50002, 2),
+('wg-03', 'گیاه خواری', 50003, 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
